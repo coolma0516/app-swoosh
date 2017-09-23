0822d1730e3497f1c978b9e9c9328018fa8f8d8a
Jian Ma <rosegun38@163.com>
2017-09-23 16:59:54 +0800

    Add the welcome screen.

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Jian Ma on 2017/9/23.
+//  Copyright © 2017年 Jian Ma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+
+}
+
---
+++ app-swoosh/app-swoosh/Views/BoarderButton.swift
@@ -0,0 +1,20 @@
+//
+//  BoarderButton.swift
+//  app-swoosh
+//
+//  Created by Jian Ma on 2017/9/23.
+//  Copyright © 2017年 Jian Ma. All rights reserved.
+//
+
+import UIKit
+
+class BoarderButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 3.0
+        layer.borderColor = UIColor.white.cgColor
+    }
+    
+    
+}
