03a13c4e397eafacc05c95426224362fa4171459
Jian Ma <rosegun38@163.com>
2017-09-25 10:21:42 +0800

    Add the model and second vc.

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Jian Ma on 2017/9/24.
+//  Copyright © 2017年 Jian Ma. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeage: String!
+    var selectedSkillLevel: String!
+}
